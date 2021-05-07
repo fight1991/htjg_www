e17f3690e32672ec18c97f78b4e958c006e15068
fight1991 <359193813@qq.com>
2021-05-07 18:04:10 +0800

    暂时迁移

--- src/pages/main.vue
+++ src/pages/main.vue
@@ -1,26 +1,28 @@
 <template>
-  <div id="app">
+  <div>
     <!-- 头部导航区域 -->
     <div class="header-navigator">
       <navigator></navigator>
     </div>
     <!-- 页面模块 -->
     <div class="pages-module">
-      <keep-alive>
-        <router-view/>
-      </keep-alive>
+      <router-view/>
     </div>
     <!-- 底部区域 -->
-    
+    <div>
+      <nav-footer></nav-footer>
+    </div>
   </div>
 </template>
 
 <script>
 import navigator from '../components/navigator'
+import navFooter from '../components/footer'
 export default {
   name: 'App',
   components: {
-    navigator
+    navigator,
+    navFooter
   }
 }
 </script>
