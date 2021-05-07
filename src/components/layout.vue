66b40d974a85d5757e515ebe071ce3a887eeb484
fight1991 <359193813@qq.com>
2021-05-07 15:59:42 +0800

    暂存

--- src/components/layout.vue
+++ src/components/layout.vue
@@ -268,7 +268,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
-  .box {
-    margin-top: 70px;
-  }
+  @import './styles/layout.less';
 </style>
