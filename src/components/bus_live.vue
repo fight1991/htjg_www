d6730f08fe11ab9b89ce4da2d80149bdb4081c35
fight1991 <359193813@qq.com>
2021-08-20 17:13:36 +0800

    典型案例区域暂存

--- src/components/bus_live.vue
+++ src/components/bus_live.vue
@@ -11,36 +11,69 @@
         </div>
       </div>
     </div>
-    <!-- 机动车电子标识应用方向区域 -->
-    <div class="elec-area common-area">
-      <div class="title aniview" data-av-animation="flipInX">
-        <img src="../assets/images/bus_live/title1.png" alt="">
+    <div class="banxin flex-row">
+      <div class="plant-content">
+        <div class="content-box">
+          <div class="text-main">车管理服务</div>
+          <div class="pos-text-sub">
+            <p>商圈、公共停车场等智能停车管理</p>
+            <p>重要机关、企事业单位及特殊通道等车辆出入管理</p>
+            <p>园区、厂区等一卡通行</p>
+            <p>车辆进出数据统计分析</p>
+          </div>
+        </div>
       </div>
-      <div class="banxin grid-icon-box">
-        <div class="grid-item">
-          <img src="../assets/images/bus_live/grid1.png" alt="">
+      <div class="plant-img pos-img">
+        <img src="../assets/images/business/life-picture1.png" alt="">
+      </div>
+    </div>
+    <div class="banxin flex-row">
+      <div class="plant-img pos-img">
+        <img src="../assets/images/business/life-picture2.png" alt="">
+      </div>
+      <div class="plant-content">
+        <div class="content-box">
+          <div class="text-main">车金融服务</div>
+          <div class="pos-text-sub">
+            <p>刷车支付：无感加油、便捷洗车、4S店、汽车美容等涉车商业场景下的刷车交易</p>
+            <p>消费信贷：打造车呗产品，实现涉车小额消费授信</p>
+            <p>精准保险：保险销售、快处快赔、保险产品设计等</p>
+            <p>路桥费、车船税等规税费电子化征稽查验</p>
+          </div>
         </div>
-        <div class="grid-item">
-          <img src="../assets/images/bus_live/grid2.png" alt="">
+      </div>
+    </div>
+    <div class="banxin flex-row">
+      <div class="plant-content">
+        <div class="content-box">
+          <div class="text-main">车信息服务</div>
+          <div class="pos-text-sub">
+            <p>面向市民：通过手机APP、门户网站、交通诱导屏、电子站牌、电台等方式提供丰富的交通出行信息</p>
+            <p>面向商户：获取车辆精准数据，方便涉车商户为车主进行精准营销与服务</p>
+          </div>
         </div>
-        <div class="grid-item">
-          <img src="../assets/images/bus_live/grid3.png" alt="">
+      </div>
+      <div class="plant-img pos-img">
+        <img src="../assets/images/business/life-picture3.png" alt="">
+      </div>
+    </div>
+    <!-- 典型案例区域 -->
+    <div class="eg">
+      <div class="eg-title banxin">
+        <img src="../assets/images/business/eg-title.png" alt="">
+      </div>
+      <div class="content banxin">
+        <div class="case">
+          <img src="../assets/images/business/life-case.png" alt="">
         </div>
-        <div class="grid-item">
-          <img src="../assets/images/bus_live/grid4.png" alt="">
-        </div>
-        <div class="grid-item">
-          <img src="../assets/images/bus_live/grid5.png" alt="">
-        </div>
-        <div class="grid-item">
-          <img src="../assets/images/bus_live/grid6.png" alt="">
-        </div>
-        <div class="grid-item">
-          <img src="../assets/images/bus_live/grid7.png" alt="">
-        </div>
-        <div class="grid-item">
-          <img src="../assets/images/bus_live/grid8.png" alt="">
-        </div>
+        <!-- <div class="case case-4">
+          <img src="../assets/images/business/life-picture4.png" alt="">
+        </div> -->
+      </div>
+    </div>
+    <div class="eg2 banxin">
+      <div class="case-4">
+        <img src="../assets/images/business/life-picture4.png" alt="">
       </div>
     </div>
   </div>
