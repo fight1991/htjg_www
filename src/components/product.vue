4ccbbf5ceaf59481b13a93923ef804bfe410e194
fight1991 <359193813@qq.com>
2021-08-24 13:54:29 +0800

    产品中心暂存

--- src/components/product.vue
+++ src/components/product.vue
@@ -119,6 +119,110 @@
       </div>
     </div>
    <!-- 软件产品区域 -->
+    <div id="solf">
+      <div class="title">
+        <img src="../assets/images/product/soft-title.png" alt="">
+      </div>
+      <div class="content">
+        <div class="flow-line bg-e">
+          <div class="banxin flow-line-item">
+            <div class="text-info">
+              <div class="one-title">交通出行平台</div>
+              <div class="line-gap"></div>
+              <div class="one-title">城市交通出行更加便捷</div>
+              <div class="one-title">用全域一张网提升便民利民服务</div>
+              <div class="sub-line">
+                <p>交通诱导，提高全市交通出行效率</p>
+                <p>智能交通信号管理</p>
+                <p>公交车、医护车辆等特种车优先</p>
+                <p>无感支付，快速通行</p>
+                <p>停车一张网，诱导、共享</p>
+                <p>快进快出，先离场后付费</p>
+                <p>降低用车成本</p>
+              </div>
+            </div>
+            <div class="img-info">
+              <img src="../assets/images/product/product-software1.png" alt="">
+            </div>
+          </div>
+        </div>
+        <div class="flow-line">
+          <div class="banxin flow-line-item">
+            <div class="text-info">
+              <div class="one-title">交通智慧中心</div>
+              <div class="line-gap"></div>
+              <div class="one-title">城市交通治理更加高效</div>
+              <div class="one-title">用交通大数据赋能公安交管</div>
+              <div class="sub-line">
+                <p>精准核查机动车逾期未检验、逾期未报废等信息，并发出执法预警</p>
+                <p>精准核查危化品车行驶时间和路线</p>
+                <p>核实确认假牌、套牌等违法车辆</p>
+                <p>精准校准卡口设备准确性</p>
+                <p>两客一危管控</p>
+                <p>环保限行</p>
+                <p>通行证管理</p>
+              </div>
+            </div>
+            <div class="img-info">
+              <img src="../assets/images/product/product-software2.png" alt="">
+            </div>
+          </div>
+        </div>
+        <div class="flow-line bg-e">
+          <div class="banxin flow-line-item">
+            <div class="text-info">
+              <div class="one-title">车生活服务网络平台</div>
+              <div class="line-gap"></div>
+              <div class="one-title">城市车生活各类商业化场景应用</div>
+              <div class="one-title">以平台赋能商家提供优质服务</div>
+              <div class="sub-line">
+                <p>无感加油</p>
+                <p>便民4S店、洗车、保养等服务</p>
+                <p>精准车险产品细化</p>
+                <p>简易事故快处快赔、快处直赔</p>
+                <p>基于车主信用数据的金融借贷</p>
+              </div>
+            </div>
+            <div class="img-info">
+              <img src="../assets/images/product/product-software3.png" alt="">
+            </div>
+          </div>
+        </div>
+        <div class="flow-line">
+          <div class="banxin flow-line-item">
+            <div class="area-info">
+              <div class="left">
+                <div class="one-title">小程序展示</div>
+                  <div class="line-gap"></div>
+                  <div class="one-title">吉速登记</div>
+                  <div class="sub-title">非机动车管控小程序</div>
+                  <div class="sub-img">
+                    <img src="../assets/images/product/product-interfaceicon1.png" alt="">
+                  </div>
+              </div>
+              <div class="right">
+                <img src="../assets/images/product/product-interface1.png" alt="">
+              </div>
+            </div>
+            <div class="gap"></div>
+            <div class="area-info">
+              <div class="left">
+                <div class="one-title">小程序展示</div>
+                  <div class="line-gap"></div>
+                  <div class="one-title">吉光车生活</div>
+                  <div class="sub-title">机动车电子标识应用小程序</div>
+                  <div class="sub-img">
+                    <img src="../assets/images/product/product-interfaceicon2.png" alt="">
+                  </div>
+              </div>
+              <div class="right">
+                <img src="../assets/images/product/product-interface2.png" alt="">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
